ab0f1ea330ab8da8bb22c6f58da26365b1484659
Alex Assis <alex_assis17@hotmail.com>
2025-08-25 22:27:05 -0300

    Adiciona ESLint e atualiza arquivos modificados

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+    'no-console': 'warn',
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+  },
+};
